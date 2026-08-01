47665b9b796315d1634da26a20d245b342f8387b
Rishu <suryodipta@gmail.com>
2026-08-02 01:50:19 +0530

    added pkl file

---
+++ Backend/server/services/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+ta(data : Data):
+#     Region = data.country if data.country in top_countries else "Other"
+#     input_row = pd.DataFrame([{
+#             'Age'                       :data.age,
+#             'Gender'                    :data.gender,
+#             'Region'                    :Region,
+#             'Academic_Level'            :data.academic_level,
+#             'Most_Used_Platform'        :data.most_used_platform,
+#             'Purpose_Of_Use'            :data.purpose_of_use,
+#             'Avg_Daily_Usage_Hours'     :data.avg_daily_usage_hours,
+#             'Daily_Unlocks'             :data.daily_unlocks,
+#             'Study_Hours'               :data.study_hours,
+#             'Physical_Activity_Hours'   :data.physical_activity_hours,
+#             'Sleep_Hours_Per_Night'     :data.sleep_hours_per_night,
+#             'Stress_Level'              :data.stress_level,
+#        }])
+#     prediction = model.predict(input_row)[0]
+#     return PredictionResponse(pr
